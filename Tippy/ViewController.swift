ad1d0b5c6fc8c71640e4fc33b38520a94ebb0119
Diana Fisher <diana.fisher@gmail.com>
2017-08-10 12:20:30 -0700

    Initial project creation following video walkthrough

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Diana Fisher on 8/10/17.
+//  Copyright © 2017 Diana Fisher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func onTap(_ sender: Any) {
+        print("tapped!")
+        
+        view.endEditing(true) // dismiss the keyboard
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        // set the values of tip and total
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let selectedPercent = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        let bill = Double(billTextField.text!) ?? 0
+        let tip = bill * selectedPercent
+        let total = bill + tip
+        
+        // convert the String to a Double or use 0, if it is nil
+//        tipLabel.text = "$\(tip)"
+//        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+    }
+}
+
